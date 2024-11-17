8265c44c604bbf54ff20f71b797f753242959175
yejing <yejing@bjtu.edu.cn>
2024-11-17 12:49:06 +0800

    Update Person2.java

--- java/object-alignment/src/main/java/Person2.java
+++ java/object-alignment/src/main/java/Person2.java
@@ -1,10 +1,5 @@
 public class Person2 {
-    public byte _byte;
-    public boolean _boolean;
     public char _char;
     public short _short;
     public int _int;
-    public long _long;
-    public float _float;
-    public double _double;
 }
