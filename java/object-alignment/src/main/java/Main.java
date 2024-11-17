de913f7a4d39a7cee231414fbd9e0ac3622647e2
yejing <yejing@bjtu.edu.cn>
2024-11-17 12:49:38 +0800

    Update Main.java

--- java/object-alignment/src/main/java/Main.java
+++ java/object-alignment/src/main/java/Main.java
@@ -5,5 +5,9 @@ public class Main {
         Person person = new Person();
         String s = ClassLayout.parseInstance(person).toPrintable();
         System.out.println(s);
+
+        Person2 person2 = new Person2();
+        String s2 = ClassLayout.parseInstance(person2).toPrintable();
+        System.out.println(s2);
     }
 }
